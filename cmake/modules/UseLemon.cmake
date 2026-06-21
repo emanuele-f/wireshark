f3875c3ebbeba7d5e7af5987efe69e9f2e9372e2
emanuele-f <black.silver@hotmail.it>
2026-06-21 17:19:17 +0200

    Allow building with external lemon
    This fixes cross-compilation, as lemon needs to be run
    on the build machine

--- cmake/modules/UseLemon.cmake
+++ cmake/modules/UseLemon.cmake
@@ -1,14 +1,20 @@
 # Create macros for using the lemon parser generator.
 
-# If we're cross-compiling and /usr/share/lemon/lempar.c exists, try to
-# find the system lemon and use it. We need to build our own lemon
-# otherwise.
-if (CMAKE_CROSSCOMPILING AND EXISTS /usr/share/lemon/lempar.c)
-	find_program(LEMON_EXECUTABLE lemon)
+SET(LEMON_DEP "")
+
+if( LEMON_BIN )
+   # Use externally provided lemon binary (e.g. for cross-compilation)
+   SET(LEMON_DEP "")
+elseif (CMAKE_CROSSCOMPILING AND EXISTS /usr/share/lemon/lempar.c)
+   # If we're cross-compiling and /usr/share/lemon/lempar.c exists, try to
+   # find the system lemon and use it.
+   find_program(LEMON_EXECUTABLE lemon)
+   if(LEMON_EXECUTABLE)
+      SET(LEMON_BIN ${LEMON_EXECUTABLE})
+   endif()
 endif()
 
-if(LEMON_EXECUTABLE)
-	# Use system lemon
+if(LEMON_BIN)
 	macro(generate_lemon_file _out _in)
 		add_custom_command(
 			OUTPUT
@@ -16,12 +22,14 @@ if(LEMON_EXECUTABLE)
 				# These files are generated as side-effect
 				${_out}.h
 				${_out}.out
-			COMMAND ${LEMON_EXECUTABLE}
-				-T/usr/share/lemon/lempar.c
+			COMMAND "${LEMON_BIN}"
+				-T${CMAKE_SOURCE_DIR}/tools/lemon/lempar.c
 				-d.
 				${_in}
 			DEPENDS
 				${_in}
+				${LEMON_DEP}
+				${CMAKE_SOURCE_DIR}/tools/lemon/lempar.c
 		)
 	endmacro()
 	add_custom_target(lemon)
